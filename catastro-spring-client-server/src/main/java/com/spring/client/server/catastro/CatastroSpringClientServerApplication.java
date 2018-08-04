360ff2328ad709fe566aeb48cf76ff05b0937b28
xndy_sxnchez <andysanchezgonzalez1996@gmail.com>
2018-08-04 01:38:38 -0500

    new configuration!

---
+++ catastro-spring-client-server/src/main/java/com/spring/client/server/catastro/CatastroSpringClientServerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.client.server.catastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatastroSpringClientServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatastroSpringClientServerApplication.class, args);
+	}
+}
