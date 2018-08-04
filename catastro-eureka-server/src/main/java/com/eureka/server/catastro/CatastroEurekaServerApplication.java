eafb86c8f11478b01d90a996efd83e48411f93e3
xndy_sxnchez <andysanchezgonzalez1996@gmail.com>
2018-08-03 22:59:42 -0500

    new Project !!

---
+++ catastro-eureka-server/src/main/java/com/eureka/server/catastro/CatastroEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.eureka.server.catastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatastroEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatastroEurekaServerApplication.class, args);
+	}
+}
